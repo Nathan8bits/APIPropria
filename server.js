dd795095fc61168bccc12879e7c6383ba552b2a9
Nathan8bits <natan8bits@gmail.com>
2025-04-25 20:55:42 -0300

    get, post, delete, update

--- server.js
+++ server.js
@@ -18,12 +18,52 @@ app.post('/usuarios', async (req, res) => {
         }
     })
 
+    res.status(201).json(req.body) //deu certo e criou usuario
+})
+
+app.put('/usuarios/:id', async (req, res) => {
+    console.log(req.body)
+    
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
 
     res.status(201).json(req.body) //deu certo e criou usuario
 })
 
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({message: 'usuario deletado com sucesso'})
+})
+
 app.get('/usuarios', async (req, res) => {
-    const users = await prisma.user.findMany()
+
+    let users = []
+
+    if(req.query){
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name,
+                email: req.query.email,
+                age: req.query.age
+            }
+        }) 
+    }else {
+        users = await prisma.user.findMany()
+    }
+
 
     res.status(200).json(users)
 })
